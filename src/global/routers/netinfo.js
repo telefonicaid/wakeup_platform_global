565fe3c03c608a38b333b17236b72b3a63156234
Guillermo Lopez <willyaranda@mozilla-hispano.org>
2014-02-05 22:36:40 +0100

    Feature. Issue #37. Add a netInfo/v1, which returns the data saved on process.netinfo

---
+++ src/global/routers/netinfo.js
@@ -0,0 +1,23 @@
+/* jshint node: true */
+/**
+ * Wake Up Platform
+ * (c) Telefonica Digital, 2014 - All rights reserved
+ * License: GNU Affero V3 (see LICENSE file)
+ * Fernando Rodríguez Sela <frsela at tid dot es>
+ * Guillermo López Leal <gll at tid dot es>
+ */
+
+var log = require('../shared_libs/logger');
+
+module.exports.info = {
+    name: 'netInfo',
+    type: 'router',
+    virtualpath: 'netinfo/v1',
+    description: 'Returns a JSON with the MCC-MNC networks and state'
+};
+
+module.exports.entrypoint = function netInfo(parsedURL, body, req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200;
+    res.write(JSON.stringify(process.netinfo || {}));
+};
