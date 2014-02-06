4e0cec0abdb73452da2c0d862e2a816301cc1eae
Fernando Rodriguez Sela <frsela@tid.es>
2014-02-06 12:18:28 +0100

    Updated netinfo router with last changes

--- src/routers/netinfo.js
+++ src/routers/netinfo.js
@@ -8,7 +8,7 @@
  */
 
 var log = require('../shared_libs/logger'),
-    mn = require('../libs/mobile_networks.js');
+    mn = require('../libs/mobile_networks.js')('../networks.json');
 
 module.exports.info = {
   name: 'netInfo',
@@ -18,7 +18,21 @@ module.exports.info = {
 };
 
 module.exports.entrypoint = function netInfo(parsedURL, body, req, res) {
+  var networks = mn.getAllNetworks();
+  var netids = Object.keys(networks);
+
+  var result = [];
+
+  for (var i=0; i < netids.length; i++) {
+    result.push({
+      netid: netids[i],
+      mccmnc: networks[netids[i]].network,
+      range: networks[netids[i]].range,
+      offline: networks[netids[i]].offline
+    });
+  }
+
   res.setHeader('Content-Type', 'application/json');
   res.statusCode = 200;
-  res.write(JSON.stringify(mn.getNetworkStatuses() || {}));
+  res.write(JSON.stringify(result));
 };
